099a90ccf4115853f2a9b3927669458bd74ca040
Stanislav Kositsyn <k.stanyslaw@gmail.com>
2025-06-21 15:17:42 +0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'BallsWaveGearingGeneratorWeb',
+  webDir: 'www'
+};
+
+export default config;
