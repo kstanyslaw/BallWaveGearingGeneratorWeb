68089ca718aaa3a2fae445affab1f97c0816d0f6
Stanislav Kositsyn <k.stanyslaw@gmail.com>
2025-06-23 11:10:11 +0500

    (feat)[interfaces] create basic params interface

---
+++ src/app/interfaces/basic-params.ts
@@ -0,0 +1,14 @@
+export interface BasicParams {
+  i: number;
+  e: number;
+  rd: number;
+  Rout: number;
+  Rin: number;
+  zg: number;
+  zsh: number;
+  dsh: number;
+  hc: number;
+  Rsep_m: number;
+  Rsep_in: number;
+  Rsep_out: number;
+}
