b0ecdb1013a74060b4f12765b5edb2d7358adeaa
Stanislav Kositsyn <k.stanyslaw@gmail.com>
2025-06-25 14:07:30 +0500

    (feat)[home] add reactive form

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,8 @@
 import { Component, inject } from '@angular/core';
 import { RefresherCustomEvent } from '@ionic/angular';
 import { InputDataService } from '../services/input-data.service';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { InputField } from '../interfaces/input-field';
 
 @Component({
   selector: 'app-home',
@@ -10,7 +12,11 @@ import { InputDataService } from '../services/input-data.service';
 })
 export class HomePage {
   private inputDataService = inject(InputDataService);
-  constructor() {}
+  paramsForm!: FormGroup;
+  inputFields!: InputField[];
+  inputFlags!: InputField[];
+
+  constructor(private fb: FormBuilder) {}
 
   refresh(ev: any) {
     setTimeout(() => {
@@ -18,7 +24,33 @@ export class HomePage {
     }, 3000);
   }
 
-  get inputFields () {
-    return this.inputDataService.InputFields;
+  ngOnInit() {
+    this.inputFields = this.inputDataService.InputFields;
+    this.inputFlags = this.inputDataService.InputFlags;
+    this.paramsForm = new FormGroup({});
+    this.initForm();
+  }
+
+  onSubmit(): void {
+    console.log(this.paramsForm.value);
+  }
+
+  initForm() {
+    const formGroup : any = {};
+    this.inputFields.map((field: InputField) => {
+      formGroup[field.formControlName] = [ field.placeholder, Validators.required ];
+    });
+    this.inputFlags.map((flag: InputField) => {
+      formGroup[flag.formControlName] = [ flag.placeholder, Validators.required ];
+    })
+    this.paramsForm = this.fb.group(formGroup);
+  }
+
+  getControl(name: string): FormControl {
+    const control = this.paramsForm.get(name);
+    if (!control) {
+      throw new Error(`Form control ${name} not found`);
+    }
+    return control as FormControl;
   }
 }
