456090e40f50b4131ce3280994333e115a0487cd
Stanislav Kositsyn <k.stanyslaw@gmail.com>
2025-07-10 18:13:11 +0500

    (feat)[render] add interface for render input data

--- src/app/services/render.service.ts
+++ src/app/services/render.service.ts
@@ -1,5 +1,23 @@
 import { Injectable } from '@angular/core';
 
+interface WheelProfileConfig {
+  BASE_WHEEL_SHAPE: boolean;
+  SEPARATOR: boolean;
+  ECCENTRIC: boolean;
+  BALLS: boolean;
+  OUT_DIAMETER: boolean;
+  xy: [number, number][];
+  Rout: number;
+  Rin: number;
+  Rsep_out: number;
+  Rsep_in: number;
+  e: number;
+  rd: number;
+  zsh: number;
+  rsh: number;
+  D: number;
+}
+
 @Injectable({
   providedIn: 'root'
 })
